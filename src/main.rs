3478a706e6fcd5250dea876173872a99377fbb95
Anthony Iszer <aiszler29@gmail.com>
2023-04-05 14:55:47 -0700

    adjust var names

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+
+    println!("Lets play a game!");
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -10,20 +12,20 @@ fn main() {
     loop {
         println!("Please input your guess.");
 
-        let mut guess = String::new();
+        let mut guess_number = String::new();
 
         io::stdin()
-            .read_line(&mut guess)
+            .read_line(&mut guess_number)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess_number: u32 = match guess_number.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
+        println!("You guessed: {}", guess_number);
 
-        match guess.cmp(&secret_number) {
+        match guess_number.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
